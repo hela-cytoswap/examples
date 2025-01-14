8b8e70514d872a5a94be9e0e7fbe82314621ef63
phu.huynh <phu.huynh@vn.snapresearch.io>
2025-01-14 10:49:15 +0700

    refactor: remove gas fee constants from code and update README for mainnet RPC URL instructions

--- v3-sdk/modifying-position/src/libs/liquidity.ts
+++ v3-sdk/modifying-position/src/libs/liquidity.ts
@@ -13,15 +13,18 @@ import { BigNumber, ethers } from 'ethers'
 import { CurrentConfig } from '../config'
 import {
   ERC20_ABI,
-  MAX_FEE_PER_GAS,
-  MAX_PRIORITY_FEE_PER_GAS,
   NONFUNGIBLE_POSITION_MANAGER_ABI,
   NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS,
   TOKEN_AMOUNT_TO_APPROVE_FOR_TRANSFER,
 } from './constants'
 import { fromReadableAmount } from './conversion'
 import { getPoolInfo } from './pool'
-import { getProvider, getWalletAddress, sendTransaction, TransactionState } from './providers'
+import {
+  getProvider,
+  getWalletAddress,
+  sendTransaction,
+  TransactionState,
+} from './providers'
 
 export interface PositionInfo {
   tickLower: number
@@ -77,8 +80,6 @@ export async function addLiquidity(
     to: NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS,
     value: value,
     from: address,
-    maxFeePerGas: MAX_FEE_PER_GAS,
-    maxPriorityFeePerGas: MAX_PRIORITY_FEE_PER_GAS,
   }
 
   return sendTransaction(transaction)
@@ -142,8 +143,6 @@ export async function removeLiquidity(
     to: NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS,
     value: value,
     from: address,
-    maxFeePerGas: MAX_FEE_PER_GAS,
-    maxPriorityFeePerGas: MAX_PRIORITY_FEE_PER_GAS,
   }
 
   return sendTransaction(transaction)
@@ -322,8 +321,6 @@ export async function mintPosition(): Promise<TransactionState> {
     to: NONFUNGIBLE_POSITION_MANAGER_CONTRACT_ADDRESS,
     value: value,
     from: address,
-    maxFeePerGas: MAX_FEE_PER_GAS,
-    maxPriorityFeePerGas: MAX_PRIORITY_FEE_PER_GAS,
   }
 
   return sendTransaction(transaction)
